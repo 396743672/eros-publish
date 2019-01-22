333ad42068690bc29fea6a930f4a53d247e77622
long <396743672@qq.com>
2019-01-22 18:02:38 +0800

    配置

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	db:'mongodb://localhost:27017/app',
 	defaultPort: 3001,
 	staticVirtualPath: '/static',
-	staticRealPath: '/Users/yangmingzhe/Work/opensource/',
+	staticRealPath: 'G:/sunlikeSvn/',
 
 	zipReturn: 'diff' // full 全量
 }
